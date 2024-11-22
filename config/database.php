9e6a0a57bb88f540a77b8e0778151ca6246d3ac2
Harry Boyes <harry.boyes@gmail.com>
2024-11-22 14:31:49 +0000

    Remove DB config from gitignore and load with env variables

---
+++ config/database.php
@@ -0,0 +1,8 @@
+<?php
+
+$database = array(
+    'address'  => getenv('MYSQL_HOST'),
+    'username' => getenv('MYSQL_USER'),
+    'password' => getenv('MYSQL_PASSWORD'),
+    'database' => getenv('MYSQL_DATABASE')
+);
